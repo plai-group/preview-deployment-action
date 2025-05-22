16de63a2922fdb4dacc7754e71e04b8a1e4ab547
Yiyuan Sun <50027648+nusnauyiy@users.noreply.github.com>
2025-05-21 23:10:59 -0700

    Update cloudfront-function.js

--- src/aws/cloudfront-function.js
+++ src/aws/cloudfront-function.js
@@ -1,4 +1,4 @@
-// Version 1.1.0
+// Version 1.2.0 cloudfront-function.js
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function handler(event) {
   const request = event.request
@@ -8,22 +8,23 @@ function handler(event) {
   const subDomain = host.split(".")[0]
   console.log("subDomain")
   console.log(subDomain)
-
   const uri = request.uri
   console.log("uri")
   console.log(uri)
-
+  
   // Check whether the URI is missing a file name.
   if (uri.endsWith("/")) {
+    // For root paths like "/" or "/some-path/"
     request.uri = `/${subDomain}${uri}index.html`
   } else if (uri.includes(".")) {
+    // For actual files like "/script.js" or "/styles.css"
     request.uri = `/${subDomain}${uri}`
   } else {
-    request.uri = `/${subDomain}/404.html`
+    // For SPA routes like "/dashboard" or "/profile" - serve index.html
+    request.uri = `/${subDomain}/index.html`
   }
-
+  
   console.log("request.uri")
   console.log(request.uri)
-
   return request
 }
